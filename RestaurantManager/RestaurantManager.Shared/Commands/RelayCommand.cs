6db221615a779961de840294b6da699c31e72d04
IvelinStanchev <telerika123@gmail.com>
2014-11-20 18:16:29 +0200

    Created menu page

---
+++ RestaurantManager/RestaurantManager.Shared/Commands/RelayCommand.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace RestaurantManager.Commands
+{
+    class RelayCommand : ICommand
+    {
+        Func<bool> canExecute;
+        Action executeAction;
+
+        public RelayCommand(Action executeAction)
+            : this(executeAction, null)
+        {
+        }
+
+        public RelayCommand(Action executeAction, Func<bool> canExecute)
+        {
+            if (executeAction == null)
+            {
+                throw new ArgumentNullException("executeAction");
+            }
+
+            this.executeAction = executeAction;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            bool result = true;
+            Func<bool> canExecuteHandler = this.canExecute;
+            if (canExecuteHandler != null)
+            {
+                result = canExecuteHandler();
+            }
+
+            return result;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public void RaiseCanExecuteChanged()
+        {
+            EventHandler handler = this.CanExecuteChanged;
+            if (handler != null)
+            {
+                handler(this, new EventArgs());
+            }
+        }
+
+        public void Execute(object parameter)
+        {
+            this.executeAction();
+        }
+    }
+}
---
+++ RestaurantManager/RestaurantManager.Shared/ViewModels/MenuViewModel.cs
@@ -0,0 +1,80 @@
+﻿using RestaurantManager.Commands;
+using RestaurantManager.Views;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace RestaurantManager.ViewModels
+{
+    public class MenuViewModel
+    {
+        private ICommand goToAddOrdersCommand;
+        private ICommand goToFinishedOrdersCommand;
+        private ICommand goToAllOrdersCommand;
+        private ICommand exitApplicationCommand;
+
+        public MenuViewModel()
+        {
+            this.goToAddOrdersCommand = new RelayCommandWithParameter(this.GoToAddOrdersAction);
+            this.goToFinishedOrdersCommand = new RelayCommand(this.GoToFinishedOrdersAction);
+            this.goToAllOrdersCommand = new RelayCommand(this.GoToAllOrdersAction);
+            this.exitApplicationCommand = new RelayCommand(this.ExitApplicationAction);
+        }
+
+        public ICommand GoToAddOrdersCommand
+        {
+            get
+            {
+                return this.goToAddOrdersCommand;
+            }
+        }
+
+        public ICommand GoToFinishedOrdersCommand
+        {
+            get
+            {
+                return this.goToFinishedOrdersCommand;
+            }
+        }
+
+        public ICommand GoToAllOrdersCommand
+        {
+            get
+            {
+                return this.goToAllOrdersCommand;
+            }
+        }
+
+        public ICommand ExitApplicationCommand
+        {
+            get
+            {
+                return this.exitApplicationCommand;
+            }
+        }
+
+        private void GoToAddOrdersAction(object parameter)
+        {
+            var frame = ((Frame)Window.Current.Content);
+            frame.Navigate(typeof(FinishedOrdersPage), 0);
+        }
+
+        private void GoToFinishedOrdersAction()
+        {
+        }
+
+        private void GoToAllOrdersAction()
+        {
+        }
+
+        private void ExitApplicationAction()
+        {
+            App.Current.Exit();
+        }
+    }
+}
